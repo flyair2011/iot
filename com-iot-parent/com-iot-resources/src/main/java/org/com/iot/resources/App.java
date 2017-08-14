5ff796ee7bd8e10fa300cd6a7e3cd2943ca1a2ae
flyair2011 <1079749397@qq.com>
2017-08-14 22:26:14 +0800

    init submit

---
+++ com-iot-parent/com-iot-resources/src/main/java/org/com/iot/resources/App.java
@@ -0,0 +1,13 @@
+package org.com.iot.resources;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
